84771e17a1a1fd9cb4aa3dcb863a6092fc0fb11a
zhangBo121 <1972625191@qq.com>
2020-11-27 00:51:51 +0800

    购物车部分功能及登录注册优化

--- js/goodsCar.js
+++ js/goodsCar.js
@@ -18,10 +18,12 @@ if (localStorage.getItem('goods')) {
     document.querySelector('.main').firstElementChild.appendChild(div)
 }
 
+
+
 function dataDemand() {
     var main = document.querySelector('.main') //商品中最外层的大盒子
 
-
+    // ajax请求
     ajax({
         url: '../data/goodslist.json',
         type: 'get',
@@ -41,16 +43,19 @@ function dataDemand() {
                 json.forEach((obj) => { //json 遍历后每个数据都是一个对象
                     if (item.id === obj.id) { //判断两个数据中的id是否一致
                         // 数据一致就可以渲染到页面上
-                        console.log(item.num);
-                        console.log(obj.price);
-                        console.log(obj.price.slice(1));
+                        // console.log(item.num);、
+                        // console.log(obj.price);
+                        // console.log(obj.price.slice(1));
+                        // 把数据中的价格字符串转成数字
                         var prices = (obj.price.slice(1) - 0)
-                        console.log(prices);
+                            // console.log(prices);
+
                         // 创建一个包裹商品的div
                         var div = document.createElement('div')
 
                         // 给创建的div 添加类名 goodS
                         div.className = 'goodS'
+                        div.index = item.id
 
                         // 给创建的div 添加内容
                         div.innerHTML += `
@@ -66,12 +71,12 @@ function dataDemand() {
                             <div class="m_right">
                                 <b>${obj.price}</b>
                                 <div class="num">
-                                    <b>-</b>
+                                    <b class='minus'>-</b>
                                     <em>${item.num}</em>
-                                    <i>+</i>
+                                    <i class='add'>+</i>
                                 </div>
                                 <strong>${prices*(item.num)}</strong>
-                                <span class="iconfont icon-lajitong"></span>
+                                <em><i class='rem'>删除</i></em>
                             </div>
                         </div>
                         <div class="goods_foot">
@@ -90,4 +95,291 @@ function dataDemand() {
 
         }
     })
+
+    // 判断数组中是否包含某个值
+    function has(arr, item) {
+        for (var i = 0, len = arr.length; i < len; i++) {
+            if (arr[i] === item) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 请求完成，开始事件委托
+    var wrap = document.querySelector('.wrap') //页面中最大的盒子
+    var all = document.querySelector('.all') //全选
+    var del2 = document.querySelector('.del2') //删除选中的商品的盒子
+
+    wrap.onclick = function(e) { //事件委托
+        var target = e.target // 
+            // console.log(target, 'target');
+
+        // 加号  需要清除本地数据
+        if (target.className == 'add') {
+            // 原始数量
+            var num1 = target.previousElementSibling.innerHTML - 0 //把获取到的字符串转成数字
+
+            // 小计
+            var amounts = target.parentElement.nextElementSibling.innerHTML - 0 //金额增加
+
+            // 单价
+            var price = target.parentElement.previousElementSibling.innerHTML.slice(1) - 0 //获取价格
+
+            // 商品金额 target.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.lastElementChild.innerHTML
+
+            let div = target.parentElement.parentElement.parentElement.parentElement //商品最大的盒子
+            let data1 = JSON.parse(localStorage.getItem('goods')) //每次操作数据都需要重新请求
+
+
+            num1++ //每点击一次数量就加 1
+            // 判断加入购物车的数量是否达到上限
+            if (num1 > 10) {
+                num1 = 10
+                alert('购买的商品数量已达上限')
+            }
+
+            // 从当前数据中找到该下标的数据，并删除 
+            data1.forEach((item, index) => {
+                if (item.id === div.index) {
+                    item.num = num1
+                }
+            })
+
+            // 把加 后的数据重新加入到localstorage 
+            localStorage.setItem('goods', JSON.stringify(data1))
+
+
+            // 小计 = 数量 * 单价
+            amounts = num1 * price
+
+            // 将得到的值重新赋值给原来的盒子  更新值
+            target.previousElementSibling.innerHTML = num1
+            target.parentElement.nextElementSibling.innerHTML = amounts
+            target.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.lastElementChild.innerHTML = amounts
+
+            total() //计算总价
+        }
+
+        // 减号  需要清除本地数据
+        if (target.className == 'minus') {
+            // 原始数量
+            var num2 = target.nextElementSibling.innerHTML - 0 //把获取到的字符串转成数字
+
+            // 小计
+            var amounts = target.parentElement.nextElementSibling.innerHTML - 0 //小计金额增加
+
+            // 单价
+            var price = target.parentElement.previousElementSibling.innerHTML.slice(1) - 0 //获取价格
+
+            // 商品金额  target.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.lastElementChild.innerHTML
+
+            let div = target.parentElement.parentElement.parentElement.parentElement //商品最大的盒子
+            let data2 = JSON.parse(localStorage.getItem('goods')) //每次操作数据都需要重新请求
+
+            num2-- //每点击一次数量就加 1
+            // 判断加入购物车的数量是否达到上限
+            if (num2 <= 0) {
+                num2 = 1
+                alert('购买的商品数量已达最低')
+            }
+
+            // 从当前数据中找到该下标的数据，并删除 
+            data2.forEach((item, index) => {
+                // item.id === div.index && data.splice(index, 1)
+                if (item.id === div.index) {
+                    item.num = num2
+                }
+
+            })
+
+            // 把减 后的数据重新加入到localstorage 
+            localStorage.setItem('goods', JSON.stringify(data2))
+
+
+            // 小计 = 数量 * 单价  = 商品金额
+            amounts = num2 * price
+
+            // 将得到的值重新赋值给原来的盒子  更新值
+            target.nextElementSibling.innerHTML = num2
+
+            // 将累加的金额赋值给小计
+            target.parentElement.nextElementSibling.innerHTML = amounts
+
+            // 将小计金额赋值给商品金额
+            target.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.lastElementChild.innerHTML = amounts
+
+            total() //计算总价
+        }
+
+        // 删除该商品  需要清除本地数据
+        if (target.className == 'rem') {
+            let div = target.parentElement.parentElement.parentElement.parentElement //商品最大的盒子
+            let data = JSON.parse(localStorage.getItem('goods')) //每次操作数据都需要重新请求
+            console.log(div)
+            console.log(div.index)
+
+            // 从当前数据中找到该下标的数据，并删除 
+            data.forEach((item, index) => {
+                item.id === div.index && data.splice(index, 1)
+            })
+
+            // 把删除后的数据重新加入到localstorage 
+            localStorage.setItem('goods', JSON.stringify(data))
+
+            // 删除包裹整个商品的大盒子
+            target.parentElement.parentElement.parentElement.parentElement.parentElement.removeChild(target.parentElement.parentElement.parentElement.parentElement)
+
+            total() //计算总价
+        }
+
+
+        // 每个商品的勾选情况  单选
+        if (target.className == 'check01') {
+            var selectArr = [] //储存勾选状态
+            var checks = document.querySelectorAll('.check01') //获取每个商品的选择框
+
+            // 遍历所有任务的勾选状态  用来判断单选是否勾选
+            for (var i = 0; i < checks.length; i++) {
+                if (checks[i].checked) {
+                    selectArr.push('a') //a 是自定义的  可用于与 b 在数组中区分
+                } else {
+                    selectArr.push('b')
+                }
+            }
+
+            // 判断全选是否需要勾选
+            if (has(selectArr, 'b')) {
+                all.checked = false
+            } else {
+                all.checked = true
+            }
+
+            total() //计算总价
+        }
+
+
+
+        // -----------------------------------------------------------------------------------------------------------------------------
+        //1.  主要是找出当前选中商品的id 
+        // 2. 声明的div 有问题，找不到装商品的大盒子
+
+        // 清空选中的商品 --> 需要清除本地数据  可以用事件委托做
+        if (target.className == 'del2') {
+            var checks = document.querySelectorAll('.check01') //获取每个商品的选择框
+            let div = target.parentElement.parentElement.parentElement.previousElementSibling.children.item.id //商品最大的盒子
+            let data3 = JSON.parse(localStorage.getItem('goods')) //每次操作数据都需要重新请求
+                // console.log(div);  undefined
+                // console.log(div.index);  报错
+
+            // 遍历所有商品，查看是否选中
+            for (var i = 0; i < checks.length; i++) {
+                checks[i].index1 = i
+                    // 判断商品是否勾选
+                if (checks[i].checked) {
+
+                    // 从当前数据中找到该下标的数据，并删除 
+                    data3.forEach((item, index) => {
+                        console.log(item.id);
+                        item.id === div.index && data3.splice(index, 1)
+                    })
+
+                    // 把删除后的数据重新加入到localstorage 
+                    localStorage.setItem('goods', JSON.stringify(data3))
+
+                    // 删除选中的商品
+                    checks[i].parentNode.parentNode.parentNode.parentNode.removeChild(checks[i].parentNode.parentNode.parentNode)
+
+                    // 判断全选是否勾选 ，如果勾选，则去掉全选
+                    if (all.checked) {
+                        all.checked = false
+                    }
+
+                }
+            }
+
+        }
+
+
+        // -------------------------------------------------------------------------------------------------------------------------------------------------
+        // 单选选中时的情况  全选情况和这个差不多
+        // 商品应付总额
+        // var total = 0 //累加器 累加之后赋值给总计
+        // var checks = document.querySelectorAll('.check01') //获取每个商品的选择框
+
+        // //单选选中时的情况
+        // for (var i = 0; i < checks.length; i++) {
+        //     if (checks[i].checked) {
+        //         // 获取商品小计
+        //         var total2 = checks[i].parentElement.nextElementSibling.lastElementChild.previousElementSibling.innerHTML - 0
+        //         console.log(total2);
+        //         // 商品金额累加
+        //         total += total2
+
+        //         // 将累加好的金额赋值给总计
+        //         checks[i].parentElement.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.lastElementChild.firstElementChild.lastElementChild.innerHTML = total
+        //     } else {
+        //         // 如果单选一个都没有选中 直接将 0 赋值给总计
+        //         checks[i].parentElement.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.lastElementChild.firstElementChild.lastElementChild.innerHTML = 0
+
+        //     }
+        // }
+    }
+
+    // 封装计算总价的函数   删除--加号--减号--单选--全选
+    function total() {
+        var total7 = 0 //累加器 累加之后赋值给总计
+        var checks = document.querySelectorAll('.check01') //获取每个商品的选择框
+
+        for (var i = 0; i < checks.length; i++) { //遍历出所有已经勾选的单选
+            if (checks[i].checked) { //判断全选没有勾选时，其他单选是否有勾选
+                // 获取商品小计
+                var total6 = checks[i].parentElement.nextElementSibling.lastElementChild.previousElementSibling.innerHTML - 0
+
+                // 全选没勾选时，将其他已勾选的单选相加
+                total7 += total6
+            }
+        }
+        // 将已勾选的金额相加后赋值给总计
+        // checks[i].parentElement.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.lastElementChild.firstElementChild.lastElementChild.innerHTML = total7
+        document.querySelector('.total').innerHTML = total7
+    }
+
+
+    // 点击全部 --> 全选
+    all.onclick = function() {
+        var checks = document.querySelectorAll('.check01') //获取每个商品的选择框
+        for (var i = 0; i < checks.length; i++) {
+            if (this.checked) {
+                // console.log(this.checked);
+                checks[i].checked = true
+                    // console.log(22222);
+            } else {
+                checks[i].checked = false
+                    // console.log(11111);
+            }
+        }
+        total() //计算总价
+    }
+
+    // 去结算
+    var settleAccounts = document.querySelector('.myCar_right>span')
+    settleAccounts.onclick = function() {
+        var checks = document.querySelectorAll('.check01') //获取每个商品的选择框
+            // console.log(checks);
+        for (var i = 0; i < checks.length; i++) {
+            console.log(checks[i]);
+            console.log(checks[i].checked);
+            if (checks[i].checked) {
+                console.log('去结算');
+                return
+            } else {
+                console.log('请先勾选需要结算的商品');
+                // alert('请先勾选需要结算的商品')
+                return
+            }
+        }
+    }
+
+
 }
--- js/login.js
+++ js/login.js
@@ -50,145 +50,178 @@ for (var i = 0; i < h3s.length; i++) {
 // 普通登录，手机号验证
 var userPT = document.querySelector('.user01') //手机号输入框
 var passPT = document.querySelector('.pass01') //密码输入框
-var authCodePT = document.querySelector('.authCode') //验证码输入框
-var aCPt = document.querySelector('.login02>p:nth-child(3) span') //验证码框
-var btn1 = document.querySelector('.login02>p:nth-child(3) button') //验证码按钮
-var checked01 = document.querySelector('.checked01') //自动登录的复选框
 var btn2 = document.querySelector('.btn02') //登录按钮
 var phone = document.querySelector('.phone') //注册的手机号
 var pass = document.querySelector('.pass') //注册的密码
 var agree = document.querySelector('.agree') //注册按钮
-console.log(agree);
-console.log(phone);
-console.log(pass);
+var checked01 = document.querySelector('.checked01') //自动登录的复选框
 
-// 给登录按钮btn2添加点击事件
-/* btn2.onclick = function() {
-    // 获取用户输入的内容
-    var val1 = userPT.value //手机号
-    var val2 = passPT.value //密码
 
-    // 开始正则验证
-    // ^(\+861|1) 以+86或 1 开头，第二位是在3-9之间，后面有9 为数字结尾
-    var regPhone = /^(\+861|1)[3-9]\d{9}$/ //手机号
 
-    // 密码输入判断
-    if (val2.length == 6) {
-        if (!/[\`\~\!\@\#\$\%\^\&\*\(\)\_\+\=\-\:\“\"\'\,\.\/\<\>\！\￥\?\|\{\}\[\]\”]/.test(val2)) {
-            alert('111')
+// 普通登录验证码
+var authCodePT = document.querySelector('.authCode') //验证码输入框
+var aCPt = document.querySelector('.login02>p:nth-child(3) span') //验证码框
+var btn1 = document.querySelector('.login02>p:nth-child(3) button') //验证码按钮
+window.onload = function() { //页面加载完，就会有验证码在验证码框
 
-        } else {
-            alert('只能含有数字有字母')
+    // 进入页面时判断之前是否存有用户登录的账户
+    if (getCookie('username')) {
+        userPT.value = getCookie('username')
+        passPT.value = getCookie('password')
+        checked01.checked = true
+    }
+
+    // 页面一加载进来，验证码先刷新
+    aCPt.innerHTML = getVerificationCode(6)
+
+    // 点击btn1 切换下一张验证码
+    btn1.onclick = function() {
+        aCPt.innerHTML = getVerificationCode(6)
+    }
+
+    // 登录
+    btn2.onclick = function() {
+        // 获取用户输入的内容
+        var userval = userPT.value
+        var passval = passPT.value
+
+        var authCPTval = authCodePT.value //获取输入框的内容
+
+
+        // 判断用户是否勾选记住密码
+        if (checked01.checked) { //勾选了就是true
+            setCookie({ //保存用户名
+                key: 'username',
+                val: userPT.value,
+                days: 7
+            })
+            setCookie({ //保存密码
+                key: 'password',
+                val: passPT.value,
+                days: 7
+            })
+        } else { //没勾选  清除cookie中的用户名和密码
+            removeCookie('username')
+            removeCookie('password')
         }
-    } else {
-        alert('密码错误')
-    }
-
-    if (!regPhone.test(val1)) {
-        alert("手机号输入错误")
-    }
+        setCookie({ // 跳转页面后，要展示的用户名、
+            key: 'showname',
+            val: userPT.value,
+            days: 7
+        })
 
 
 
-} */
+        // 开始正则验证
+        // ^(\+861|1) 以+86或 1 开头，第二位是在3-9之间，后面有9 为数字结尾
+        var regPhone = /^(\+861|1)[3-9]\d{9}$/ //手机号
 
+        // 账号判断
+        if (regPhone.test(userval)) {
+            // 密码输入判断
+            if (passval.length == 6) {
+                if (!/[\`\~\!\@\#\$\%\^\&\*\(\)\_\+\=\-\:\“\"\'\,\.\/\<\>\！\￥\?\|\{\}\[\]\”]/.test(passval)) {
+                    // alert('登录成功')
+                    // ajax传入php检测
+                    ajax({
+                        url: `http://localhost/qian_phone/second/jmyp2/data/user.php`,
+                        // url: `../data/user.php`, //相对路径和绝对路径都行
+                        type: "post",
+                        data: {
+                            user: userval,
+                            pass: passval,
+                            type: 'login'
+                        },
+                        // dataType:'json',
+                        // cache:true,
+                        success: function(res) {
 
-// 登录
-btn2.onclick = function() {
-    // 获取用户输入的内容
-    var userval = userPT.value
-    var passval = passPT.value
+                            // 验证码
+                            if (authCPTval === aCPt.innerHTML) {
+                                // 验证码输入成功
+                                // console.log('验证码输入成功');
+                                alert(JSON.parse(res).msg)
+                                var timer = setTimeout(() => {
+                                            clearTimeout(timer);
 
-    // 开始正则验证
-    // ^(\+861|1) 以+86或 1 开头，第二位是在3-9之间，后面有9 为数字结尾
-    var regPhone = /^(\+861|1)[3-9]\d{9}$/ //手机号
+                                            // 跳转到指定页面
+                                            location.href = "./index.html";
+                                        }
 
-    // 账号判断
-    if (regPhone.test(userval)) {
-        // 密码输入判断
-        if (passval.length == 6) {
-            if (!/[\`\~\!\@\#\$\%\^\&\*\(\)\_\+\=\-\:\“\"\'\,\.\/\<\>\！\￥\?\|\{\}\[\]\”]/.test(passval)) {
-                // alert('登录成功')
-                // ajax传入php检测
-                ajax({
-                    url: `http://localhost/qian_phone/second/jmyp2/data/user.php`,
-                    // url: `../data/user.php`, //相对路径和绝对路径都行
-                    type: "post",
-                    data: {
-                        user: userval,
-                        pass: passval,
-                        type: 'login'
-                    },
-                    // dataType:'json',
-                    // cache:true,
-                    success: function(res) {
-                        alert(JSON.parse(res).msg)
-                    },
-                    error: function(err) {
-                        console.log(err)
-                    }
-                })
+                                        , 2000)
+                                    // // 跳转到指定页面
+                                    // location.href = "./index.html";
 
+                            } else {
+                                alert('验证码输入失败');
+                            }
+                        },
+                        error: function(err) {
+                            console.log(err)
+                        }
+                    })
+
+                } else {
+                    alert('只能含有数字有字母')
+                }
             } else {
-                alert('只能含有数字有字母')
+                alert('密码错误')
             }
+
         } else {
-            alert('密码错误')
+            alert("手机号输入错误")
         }
 
-    } else {
-        alert("手机号输入错误")
+
     }
 
+    // 注册
+    agree.onclick = function() {
+        // 获取用户输入的内容
+        var userval = phone.value
+        var passval = pass.value
 
-}
-
-// 注册
-agree.onclick = function() {
-    // 获取用户输入的内容
-    var userval = phone.value
-    var passval = pass.value
-
-    // 开始正则验证
-    // ^(\+861|1) 以+86或 1 开头，第二位是在3-9之间，后面有9 为数字结尾
-    var regPhone = /^(\+861|1)[3-9]\d{9}$/ //手机号
+        // 开始正则验证
+        // ^(\+861|1) 以+86或 1 开头，第二位是在3-9之间，后面有9 为数字结尾
+        var regPhone = /^(\+861|1)[3-9]\d{9}$/ //手机号
 
 
-    // 账号判断
-    if (regPhone.test(userval)) {
-        // 密码输入判断
-        if (passval.length == 6) {
-            if (!/[\`\~\!\@\#\$\%\^\&\*\(\)\_\+\=\-\:\“\"\'\,\.\/\<\>\！\￥\?\|\{\}\[\]\”]/.test(passval)) {
-                // alert('登录成功')
-                // ajax传入php检测
-                ajax({
-                    url: `../data/user.php`,
-                    type: "post",
-                    data: {
-                        user: userval,
-                        pass: passval,
-                        type: 'add'
-                    },
-                    // dataType:'json',
-                    // cache:true,
-                    success: function(res) {
-                        // console.log(res)
-                        alert(JSON.parse(res).msg)
-                    },
-                    error: function(err) {
-                        console.log(err)
-                    }
-                })
+        // 账号判断
+        if (regPhone.test(userval)) {
+            // 密码输入判断
+            if (passval.length == 6) {
+                if (!/[\`\~\!\@\#\$\%\^\&\*\(\)\_\+\=\-\:\“\"\'\,\.\/\<\>\！\￥\?\|\{\}\[\]\”]/.test(passval)) {
+                    // alert('登录成功')
+                    // ajax传入php检测
+                    ajax({
+                        url: `../data/user.php`,
+                        type: "post",
+                        data: {
+                            user: userval,
+                            pass: passval,
+                            type: 'add'
+                        },
+                        // dataType:'json',
+                        // cache:true,
+                        success: function(res) {
+                            // console.log(res)
+                            alert(JSON.parse(res).msg)
+                        },
+                        error: function(err) {
+                            console.log(err)
+                        }
+                    })
 
+                } else {
+                    alert('只能含有数字有字母')
+                }
             } else {
-                alert('只能含有数字有字母')
+                alert('密码错误')
             }
+
         } else {
-            alert('密码错误')
+            alert("手机号输入错误")
         }
 
-    } else {
-        alert("手机号输入错误")
     }
-
 }
